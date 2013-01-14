4d9db5590b645291db5f9ecf4579538e4261584f
Himanshu Shah <Himanshu1410@live.com>
2013-01-13 23:35:44 -0500

    Identifies the Printf Function in a Program

--- ModulePassDemo.cpp
+++ ModulePassDemo.cpp
@@ -10,73 +10,93 @@
 #include "llvm/ADT/APInt.h"
 #include "llvm/ADT/Twine.h"
 #include "llvm/IR/GlobalVariable.h"
+
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IRBuilder.h"
+
+
+
+
 using namespace llvm;
     
 namespace {
     struct ModulePassDemo : public ModulePass { 
     static char ID; // Pass identification
+    static int counter;
 	ModulePassDemo() : ModulePass(ID) {}    
     	virtual bool runOnModule(Module &M) {
-//============================Global Variable Declaration and Initialization========================
-
-
-GlobalVariable* counter = new GlobalVariable(/*Module=*/M,(Type*) Type::getInt32Ty (getGlobalContext()),
-/*isConstant=*/false,
-/*Linkage=*/GlobalValue::ExternalLinkage,
-/*Initializer=*/0,
-/*Name=*/"Counter");
-
-errs()<<*counter +1;
-
-
-
-
-
-errs()<<"\n\t\t================Global Variable Pointer========================\n";
-errs()<<"Global Variable Code \t\t|Variable ID \t\t|Variable Name\n";
-errs()<<counter<<"\t\t\t|"<<counter->getValueID()<<"\t\t\t|"<<*counter<<"\n";
-//errs()<<"\nValue"<<*counter.getInt();
-
-/*for (Module::const_global_iterator GI = M->global_begin(), GE = M->global_end(); GI != GE; ++GI) {
-  if (GI->hasName())
-	errs() << GI->getName() << endl; // Print name
-  	errs() << GI->getType()->getElementType() << endl; // Print Type
-}
-*/
-
-errs()<<"\n\t\t===============================================================\n";
-
-errs()<<"============Global Variable Successful";
-
-//////////////======================================================================================
-
 
 errs()<<"==========Start============\n\n\n";
 errs()<<"\n=======================Inside Module======================\n";
- 		for (Module::iterator FunctionIter = M.getFunctionList().begin(); 
-			FunctionIter != M.getFunctionList().end(); ) {
-// F is a pointer to a Function instance
-			Function *F = FunctionIter++;
-errs() << "-----------------------Function Name :" << F->getName() << "-----------------------\n";
-	errs()<<"\n==  ==  ==  ==  ==Instructions Starts==  ==  ==  ==  ==\n\n";
-		errs()<<"  OPCODE  \t| Instruction\n";
-			for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I)
-				errs()<<"  "<< I->getOpcodeName()<< "\t| "<< *I <<" \n";
-									}
-				errs()<<"\n==  ==  ==  ==  ==Instructions Ends==  ==  ==  ==  ==\n\n";
-errs() << "-----------------------Function Ends Here---------------------\n\n";
+ 		for (Module::iterator FunctionIter = M.getFunctionList().begin(); FunctionIter != M.getFunctionList().end(); ) {
+	Function *F = FunctionIter++; // F is a pointer to a Function instance
 
+errs() << "-----------------------Function Name :" << F->getName() << "-----------------------\n";
+errs()<<"\n==  ==  ==  ==  ==Instructions Starts==  ==  ==  ==  ==\n\n";
+errs()<<"  OPCODE  \t| Instruction\n";
+
+		for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I)
+		errs()<<"  "<< I->getOpcodeName()<< "\t| "<< *I <<" \n";
+
+errs()<<"\n==  ==  ==  ==  ==Instructions Ends==  ==  ==  ==  ==\n\n";
+errs() << "-----------------------Function Ends Here---------------------\n\n";
 errs() << "=======================================================================================\n";
-									
+								
 errs()<<"\n=======================Module Ends======================\n";
-	//errs()<< "Total Number of functions:" <<M.getFunctionList.getSize();
+	
 	errs() <<"Success Till Here";
-	return true;
+}
+
+errs()<<"\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+// Printing all the Load instructions in the program
+errs()<<"\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LOAD INSTRUCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+errs()<<"\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+
+   for(Module::iterator F = M.begin(), E = M.end(); F!= E; ++F)
+            {
+		
+
+                for(Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
+                {
+			
+                    ModulePassDemo::runOnBasicBlock(BB);
+                }
+	}
+errs()<<"\n-------------------------------------------------------------";
+errs()<<"\nTotal Number of Printf Statements in the Program ="<<"   "<<counter;
+errs()<<"\n-------------------------------------------------------------\n";
+
+	return false;
 		}
+
+
+
+ virtual bool runOnBasicBlock(Function::iterator &BB)
+        {
+            for(BasicBlock::iterator BI = BB->begin(), BE = BB->end(); BI != BE; ++BI)           
+            {
+
+		if( CallInst * pCall = dyn_cast<CallInst>(BI)){
+		
+		Function * pFunction = pCall->getCalledFunction();
+		std::string fname = pFunction->getName();
+		if(fname =="printf")
+		errs()<<"\n Function Name:  "<<fname<<"\t|  Function Call number:  "<<++counter;
+		}
+
+		}
+
+            return true;
+        }
+
 	};
 }
 
 char ModulePassDemo::ID = 0;
+int ModulePassDemo::counter = 0;
 static RegisterPass<ModulePassDemo> X("ModulePassDemo", "Simple Module Pass program", false, false);
 
 /**     LLVMContext &Context = M.getContext();
@@ -130,3 +150,62 @@ GlobalVariable val(*mod, (Type*) ArrayType::get(Type::getInt8Ty(getGlobalContext
 GlobalVariable *Counters = new GlobalVariable(Type::getInt8Ty(getGlobalContext()), 4, true,GlobalValue::ExternalLinkage, cons, s); 
 *Counters = 0;**/
 //Module* mod = new Module("test", getGlobalContext()); 
+
+
+
+
+//============================Global Variable Declaration and Initialization========================
+
+
+// GlobalVariable* counter = new GlobalVariable(/*Module=*/M,(Type*) Type::getInt32Ty (getGlobalContext()),
+///*isConstant=*/false,
+///*Linkage=*/GlobalValue::ExternalLinkage,
+///*Initializer=*/0,
+///*Name=*/"Counter");
+
+//errs()<<*counter+1;
+
+
+
+
+//errs()<<"\n\t\t================Global Variable Pointer========================\n";
+//errs()<<"Global Variable Code \t\t|Variable ID \t\t|Variable Name\n";
+//errs()<<counter<<"\t\t\t|"<<counter->getValueID()<<"\t\t\t|"<<*counter<<"\n";
+//errs()<<"\nValue"<<*counter.getInt();
+
+//for (Module::const_global_iterator GI = M.global_begin(), GE = M.global_end(); GI != GE; ++GI) {
+//  if (GI->hasName())
+//	errs() << GI->getName() << endl; // Print name
+//  	errs() << GI->getType()->getElementType(); // Print Type
+//}
+
+
+//errs()<<"\n\t\t===============================================================\n";
+
+//errs()<<"============Global Variable Successful============";
+
+//////////////======================================================================================
+
+/*		 if(isa<CallInst>(&(*BI)) )
+                    {
+			counter++;
+		char FName=BI.getCalledFunction().getName();
+		errs()<<"@@@@@@@Function Name:"<<FName;
+
+
+	errs()<<"\n"<< BI->getOpcodeName()<< "\t| "<< *BI ;
+
+// print the whole Function in which there is the above instruction<<" \n\t==="<<*BB<<"\n"; 
+
+                    }*/
+
+
+
+/*                            
+LoadInst *CI = dyn_cast<LoadInst>(BI);
+                            Instruction *newInst = CallInst::Create(hook, "");
+                            BB->getInstList().insert((Instruction*)CI, newInst);                         
+*/                    
+                   
+            
+
